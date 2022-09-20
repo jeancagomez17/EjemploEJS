56d32c848ce3a496d5158d9b206a5a7c08b81d81
Jean <jeancagomez94@gmail.com>
2022-09-20 11:00:10 -0500

    Style in card

--- database/data.ts
+++ database/data.ts
@@ -3,5 +3,5 @@ export const data = [
     {id: 2, name:'Andrea', profesion:"Arquitecta", imagen:"https://cdn.pixabay.com/photo/2014/03/24/17/19/teacher-295387_960_720.png"},
     {id: 3, name:'Luis', profesion:"Profesor", imagen:"https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png"},
     {id: 4, name:'Felipe', profesion:"Coordinador", imagen:"https://cdn.pixabay.com/photo/2018/08/28/13/29/avatar-3637561_960_720.png"},
-    {id: 5, name:'Andres', profesion:"Estudiante", imagen:"https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_960_720.png"},
+    {id: 5, name:'Andres Vargas', profesion:"Estudiante", imagen:"https://cdn.pixabay.com/photo/2016/03/31/20/27/avatar-1295773_960_720.png"},
 ];
